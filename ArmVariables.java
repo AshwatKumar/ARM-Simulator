518a68cbe12b78e34383a0a4f4ce47d8a31b5d70
Chandan Kumar <chandan16028@iiitd.ac.in>
2017-11-29 17:59:59 +0530

    Add files via upload

---
+++ ArmVariables.java
@@ -0,0 +1,42 @@
+package armsim; 
+public abstract class ArmVariables {
+	//registe files
+	long R[]=new long[16];
+	
+	
+
+	//memory for both instruction and allocation
+	//hex string at address
+	Long MEM_HEAP[]=new Long[4000];
+	String MEM_INST[]=new String[4000];
+
+	//control signals
+	//instuction as binary string
+	String instruction_word;
+	long operand1,operand2,answer;
+
+	//registers in instructions
+
+	int register1,register2,registerDest;
+
+	boolean branchTrue,storeTrue,loadTrue;
+	boolean isDataproc,isBranch,isDatatrans,swi_exit,swi_print,swi_read;
+	int condition,opcode,immediate;
+
+
+	abstract void swi_exit();
+	abstract void swi_print();
+	abstract void swi_read();
+	abstract String fetch();
+	abstract void decode();
+	abstract boolean execute();
+	abstract void mem();
+	abstract void write_back();
+	abstract String read_word_Instruction(int address);
+	abstract long read_word_Data(int address);
+	//write to array pointer mem+address
+	abstract void write_word_Instruction(int address,String data);
+	abstract void write_word_Data(int address,long data);
+
+	
+}
---
+++ RunArmSim.java
@@ -0,0 +1,76 @@
+package armsim;
+
+import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class RunArmSim {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		RunArmSim runArmSim = new RunArmSim();
+		Scanner in = new Scanner(System.in);
+		// System.out.println("Give name of instruction file with path");
+		// String path=in.next();
+		String path = "arraySum.mem";
+		if (path.length() == 0) {
+			System.out.println("INVALID FILE PATH PLEASE RESTART");
+			System.exit(1);
+		}
+		// reallocating all variables
+		ArmVariables armVar = new ArmSim();
+		// loading memory file into memory
+		runArmSim.load_program_memory(path, armVar);
+		// running ARMSIM
+		runArmSim.runarmsim(armVar);
+
+		in.close();
+	}
+
+	void load_program_memory(String path, ArmVariables armVar) {
+		try {
+			FileReader fileReader = new FileReader(path);
+			BufferedReader bufferedReader = new BufferedReader(fileReader);
+			String line;
+			while ((line = bufferedReader.readLine()) != null) {
+				int i = line.indexOf(" ");
+				String s1 = line.substring(2, i);
+				String instruction = line.substring(i + 1, line.length());
+				int address = Integer.parseInt(s1, 16);
+				//System.out.println(instruction+" INStruction");
+				armVar.write_word_Instruction(address, instruction);
+			}
+			fileReader.close();
+		} catch (IOException e) {
+			System.out.println("CAN'T OPEN FILE PLEASE RESTART");
+			System.exit(1);
+		}
+	}
+
+	void runarmsim(ArmVariables allArmVariables) {
+		String instruction;
+		boolean executed;
+		String zeroString=String.format("%32s", "0").replace(" ", "0");
+		// Value in PC counter is smaller than 4000 index of memory coz
+		// we have memory size 4000
+		while (allArmVariables.R[15] < 4000) {
+			instruction = allArmVariables.fetch();
+			if (instruction.equals(zeroString))
+				return;
+			
+			allArmVariables.decode();
+			
+			executed=allArmVariables.execute(); 
+			if(!executed) return;
+			  
+			allArmVariables.mem();
+			  
+			allArmVariables.write_back();
+			 
+
+		}
+	}
+
+
+}
